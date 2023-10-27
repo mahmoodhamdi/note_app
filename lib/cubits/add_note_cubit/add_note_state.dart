6156f26b892d8d6841d76f2099face0c0d44e876
mahmoodhamdi <mahmouashwah@gmail.com>
2023-10-27 02:12:18 +0200

    AddNoteState

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,16 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+sealed class AddNoteState {}
+
+final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSusses extends AddNoteState {}
+
+final class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFailure({required this.errMessage});
+}
